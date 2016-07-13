64b9f5848f4e8dc997ca48664763a8f1e68734ab
e11en <ellenlangelaar@gmail.com>
2016-07-13 16:42:14 +0200

    Added basic filtering for pokestops, gyms and a specific pokemon

--- getSighting.php
+++ getSighting.php
@@ -2,22 +2,49 @@
 include_once('db.php');
 
 if(isset($_GET['type'])){
-    if($_GET['type'] === 'pokemon'){
+    if($_GET['type'] === 'all'){
         // Return all pokemon as JSON
         echo json_encode(getSightings());
+    } else if($_GET['type'] === 'pokestop'){
+        // Return only pokestops
+        echo json_encode(getSightings(2));
+    } else if($_GET['type'] === 'gym'){
+        // Return only gyms
+        echo json_encode(getSightings(3));
+    } else if(isset($_GET['id'])){
+        echo json_encode(getSightings(null, intval($_GET['id'])));
     }
 }
 
 /**
  * Get sightings from the database
+ * @param null $type
+ * @param null $id
  * @return array
  */
-function getSightings(){
+function getSightings($type = null, $id = null){
     try{
         $pdo = getPDO();
         $sql = 'SELECT * FROM sighting';
+        if($type !== null) {
+            $sql .= ' WHERE type=:type';
+        }
+        if($id !== null){
+            $sql .= ' WHERE pokemonId=:id';
+        }
+        $stmt = $pdo->prepare($sql);
+
+        if($type !== null) {
+            $stmt->bindParam(':type', $type);
+        }
+        if($id !== null){
+            $stmt->bindParam(':id', $id);
+        }
+
+        $stmt->execute();
+        $rows = $stmt->fetchAll();
         $result = [];
-        foreach ($pdo->query($sql) as $row) {
+        foreach ($rows as $row) {
             $result[] = [$row['latitude'], $row['longitude'], $row['type']];
         }
 
