5b2856de7ac0ac19f5941f16327d3fe580c131c8
e11en <ellenlangelaar@gmail.com>
2016-07-29 15:26:12 +0200

    Forgot to change the  value name to creator

--- php/setSighting.php
+++ php/setSighting.php
@@ -1,7 +1,7 @@
 <?php
 include_once('functions.php');
 
-if(isset($_POST['pokemon']) && isset($_POST['type']) && isset($_POST['latitude']) && isset($_POST['longitude']) && isset($_POST['name'])){
+if(isset($_POST['pokemon']) && isset($_POST['type']) && isset($_POST['latitude']) && isset($_POST['longitude']) && isset($_POST['creator'])){
     echo json_encode(setSighting());
 }
 
